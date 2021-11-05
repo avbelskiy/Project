24d80fc9b8abfe5549a4e229e290c8e2e06fa8d6
avbelskiy <antonbelskij973@gmail.com>
2021-11-05 17:23:07 +0300

    test

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,8 +1,23 @@
 package com.company;
 
+import java.io.IOException;
+import java.io.FileReader;
+
 public class Main {
 
     public static void main(String[] args) {
+        try(FileReader reader = new FileReader("param.txt"))
+        {
+            // читаем посимвольно
+            int c;
+            while((c=reader.read())!=-1){
 
+                System.out.print((char)c);
+            }
+        }
+        catch(IOException ex){
+
+            System.out.println(ex.getMessage());
+        }
     }
 }
