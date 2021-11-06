1b445d707ed813280b3f2f7728e2fec1b5123252
avbelskiy <antonbelskij973@gmail.com>
2021-11-06 17:24:40 +0300

    1 alg

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,11 +1,24 @@
 package com.company;
 
 import java.util.Arrays;
+import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
         Algorytm mas = new Algorytm();
-        mas.array = new int[] {3, 1, 4, 7, 3, 9, 2, 6, 4};
-        System.out.println(Arrays.toString(mas.sort_array()));
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Введите номер алгоритма: ");
+        mas.number_alg = sc.nextInt();
+        if (mas.number_alg != 1) { System.out.println("Алгоритм с таким номером не существует.");}
+        else {
+            System.out.println("Введите длину массива: ");
+            int n = sc.nextInt();
+            mas.array = new int[n];
+            System.out.println("Введите сам массив (числа через пробел): ");
+            for (int i = 0; i < n; i++) {
+                mas.array[i] = sc.nextInt();
+            }
+            System.out.println(Arrays.toString(mas.sort_array()));
+        }
     }
 }
--- src/com/company/algorytm.java
+++ src/com/company/algorytm.java
@@ -3,21 +3,41 @@ package com.company;
 public class Algorytm {
     int number_alg;
     int[] array;
+    //String error = "";
 
     int[] sort_array() {
-        boolean isSorted = false;
-        int buf;
-        while(!isSorted) {
-            isSorted = true;
-            for (int i = 0; i < array.length-1; i++) {
-                if(array[i] > array[i+1]){
-                    isSorted = false;
-
-                    buf = array[i];
-                    array[i] = array[i+1];
-                    array[i+1] = buf;
+        switch (number_alg) {
+            case (1):
+                boolean isSorted = false;
+                int buf;
+                while (!isSorted) {
+                    isSorted = true;
+                    for (int i = 0; i < array.length - 1; i++) {
+                        if (array[i] > array[i + 1]) {
+                            isSorted = false;
+                            buf = array[i];
+                            array[i] = array[i + 1];
+                            array[i + 1] = buf;
+                        }
+                    }
                 }
-            }
+                break;
+
+            case (2):
+                for (int left = 0; left < array.length; left++) {
+                    int minInd = left;
+                    for (int i = left; i < array.length; i++) {
+                        if (array[i] < array[minInd]) {
+                            minInd = i;
+                        }
+                    }
+                    //swap(array, left, minInd);
+                }
+
+            default:
+                //error = "Алгоритм с таким номером не существует.";
+                //System.out.println(error);
+                break;
         }
         return array;
     }
