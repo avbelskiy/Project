bfa81983260812400532ffd8442ccc9c36eccb01
avbelskiy <antonbelskij973@gmail.comx>
2022-04-03 00:11:46 +0300

    Renamed

--- src/com/company/Algorithm.java
+++ src/com/company/Algorithm.java
@@ -1,6 +1,6 @@
 package com.company;
 
-public class Algorytm {
+public class Algorithm {
     int number_alg;
     int[] array;
     
--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -6,7 +6,7 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        Algorytm mas = new Algorytm();
+        Algorithm mas = new Algorithm();
         Scanner sc = new Scanner(System.in);
         System.out.println("Введите номер алгоритма: ");
         mas.number_alg = sc.nextInt();
